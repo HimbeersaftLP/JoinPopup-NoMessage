67baca3fdb6f2ec3f577593bd81207f6a82943ce
HimbeersaftLP <elias.nestl@outlook.de>
2016-04-10 12:24:50 +0200

    Update main.php

--- src/himbeer/popup/main.php
+++ src/himbeer/popup/main.php
@@ -15,9 +15,40 @@ class Main extends PluginBase implements Listener{
           $this->getLogger()->info("JoinPopup-NoMessage enabled!");
           @mkdir($this->getDataFolder());
           $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array(
+               "mode" => "popup",
                "join" => "§a-player- joined the game!",
                "quit" => "§4-player- left the game!",
           ));
           $this->saveResource("config.yml");
      }
+     public function onJoin(PlayerJoinEvent $jevent){
+          if($this->config->get("mode") == "popup"){
+               $joining = $jevent->getPlayer();
+               $pname = $joining->getName();
+               $jevent->setJoinMessage("");
+               $joinpop = str_replace("-player-", $pname, $this->coinfig->get("join"));
+               $this->getServer()->broadcastPopup($joinpop);
+          }elseif($this->config->get("mode") == "tip"){
+               $joining = $jevent->getPlayer();
+               $pname = $joining->getName();
+               $jevent->setJoinMessage("");
+               $jointip = str_replace("-player-", $pname, $this->coinfig->get("join"));
+               $this->getServer()->broadcastTip($jointip);
+          }
+	}
+     public function onQuit(PlayerQuitEvent $qevent){
+          if($this->config->get("mode") == "popup"){
+               $quitting = $qevent->getPlayer();
+               $pname = $quitting->getName();
+               $qevent->setQuitMessage("");
+               $quitpop = str_replace("-player-", $pname, $this->coinfig->get("quit"));
+               $this->getServer()->broadcastPopup($quitpop);
+	     }elseif($this->config->get("mode") == "tip"){
+               $quitting = $qevent->getPlayer();
+               $pname = $quitting->getName();
+               $qevent->setQuitMessage("");
+               $quittip = str_replace("-player-", $pname, $this->coinfig->get("quit"));
+               $this->getServer()->broadcastTip($quittip);
+	     }
+	}
 }
