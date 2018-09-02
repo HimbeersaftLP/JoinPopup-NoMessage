301a971c98e449d923f2c3db36010c6fff7bf85f
HimbeersaftLP <mail@himbeer.me>
2018-09-02 16:57:39 +0200

    Rewrite this shit

--- src/himbeer/popup/main.php
+++ src/himbeer/popup/main.php
@@ -1,54 +1,45 @@
 <?php
+
 namespace himbeer\popup;
-use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
-use pocketmine\Server;
+
 use pocketmine\event\Listener;
-use pocketmine\utils\config;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-class main extends PluginBase implements Listener{
-     
-     public function onEnable(){
-          $this->getServer()->getPluginManager()->registerEvents($this,$this);
-          $this->getLogger()->info("JoinPopup-NoMessage enabled!");
-          @mkdir($this->getDataFolder());
-          $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array(
-               "mode" => "popup",
-               "join" => "§a-player- joined the game!",
-               "quit" => "§4-player- left the game!",
-          ));
-          $this->saveResource("config.yml");
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase implements Listener {
+
+     public function onEnable(): void {
+          $this->getServer()->getPluginManager()->registerEvents($this, $this);
+          $this->saveDefaultConfig();
      }
-     public function onJoin(PlayerJoinEvent $jevent){
-          if($this->config->get("mode") == "popup"){
-               $joining = $jevent->getPlayer();
-               $pname = $joining->getName();
-               $jevent->setJoinMessage("");
-               $joinpop = str_replace("-player-", $pname, $this->config->get("join"));
-               $this->getServer()->broadcastPopup($joinpop);
-          }elseif($this->config->get("mode") == "tip"){
-               $joining = $jevent->getPlayer();
-               $pname = $joining->getName();
-               $jevent->setJoinMessage("");
-               $jointip = str_replace("-player-", $pname, $this->config->get("join"));
-               $this->getServer()->broadcastTip($jointip);
+
+     /**
+      * @param $event PlayerJoinEvent|PlayerQuitEvent
+      * @param $type string Either "join" or "quit"
+      */
+     private function handleJoinQuit($event, string $type): void {
+          $message = str_replace(
+               "{player}",
+               $event->getPlayer()->getName(),
+               $this->getConfig()->get($type)
+          );
+          if ($this->getConfig()->get("mode") === "popup") {
+               $this->getServer()->broadcastPopup($message);
+          } else {
+               $this->getServer()->broadcastTip($message);
           }
-	}
-     public function onQuit(PlayerQuitEvent $qevent){
-          if($this->config->get("mode") == "popup"){
-               $quitting = $qevent->getPlayer();
-               $pname = $quitting->getName();
-               $qevent->setQuitMessage("");
-               $quitpop = str_replace("-player-", $pname, $this->config->get("quit"));
-               $this->getServer()->broadcastPopup($quitpop);
-	     }elseif($this->config->get("mode") == "tip"){
-               $quitting = $qevent->getPlayer();
-               $pname = $quitting->getName();
-               $qevent->setQuitMessage("");
-               $quittip = str_replace("-player-", $pname, $this->config->get("quit"));
-               $this->getServer()->broadcastTip($quittip);
-	     }
-	}
+     }
+
+     public function onJoin(PlayerJoinEvent $event): void {
+          if ($this->getConfig()->get("stop_messages"))
+               $event->setJoinMessage("");
+          $this->handleJoinQuit($event, "join");
+     }
+
+     public function onQuit(PlayerQuitEvent $event): void {
+          if ($this->getConfig()->get("stop_messages"))
+               $event->setQuitMessage("");
+          $this->handleJoinQuit($event, "quit");
+     }
 }
